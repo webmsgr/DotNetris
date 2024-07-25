9fc49791f3b9ee4101286aa117dfd5b455057c57
webmsgr <zapcontact000@gmail.com>
2024-07-24 17:00:49 -0700

    feat: finish gameboard class

--- DotNetris/GameBoard.cs
+++ DotNetris/GameBoard.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 namespace DotNetris
 {
     
-    internal class GameBoard
+    public class GameBoard
     {
         public const int Width = 9;
         public const int Height = 19;
 
-        private int[] board = new int[Width*Height];
+        private Color[] board = new Color[Width*Height];
         /// <summary>
         /// Get a color at the specified position
         /// </summary>
@@ -21,7 +21,7 @@ namespace DotNetris
         /// <returns>The color at the specified position</returns>
         /// <exception cref="IndexOutOfRangeException">X/Y value outside board</exception>
 
-        public int Get(int x, int y)
+        public Color Get(int x, int y)
         {
             int index = y * Width + x;
             if (index > board.Length || index < 0)
@@ -38,7 +38,7 @@ namespace DotNetris
         /// <param name="value">Value to set</param>
         /// <exception cref="IndexOutOfRangeException">X/Y value outside board</exception>
 
-        public void Set(int x, int y, int value)
+        public void Set(int x, int y, Color value)
         {
             int index = y * Width + x;
             if (index > board.Length || index < 0)
@@ -47,9 +47,15 @@ namespace DotNetris
             }
             board[index] = value;
         }
+        /// <summary>
+        /// Clear rows, moving rows above them down.
+        /// </summary>
+        /// <param name="rows">Amount of rows to clear</param>
+        /// <param name="bottom">Bottommost row</param>
 
         public void ClearLines(int rows, int bottom)
         {
+            // TODO: please test
             // 0 1 2
             // 3 4 5
             // 6 7 8
