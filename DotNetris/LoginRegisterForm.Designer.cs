0141b73fa69e06ea8a75152b0847968dd2b16c6a
ElizabethK9 <167283322+ElizabethK9@users.noreply.github.com>
2024-08-08 13:14:21 -0700

    more design

--- DotNetris/LoginRegisterForm.Designer.cs
+++ DotNetris/LoginRegisterForm.Designer.cs
@@ -29,83 +29,229 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginRegisterForm));
-            button1 = new Button();
+            LogInBtn2 = new Button();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
+            UsernameLbl = new Label();
+            PasswordLbl = new Label();
+            LoginBtn1 = new Button();
+            OrLbl = new Label();
+            LoginLbl = new Label();
+            checkBox1 = new CheckBox();
             label1 = new Label();
             label2 = new Label();
-            button2 = new Button();
             label3 = new Label();
             label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label6 = new Label();
             SuspendLayout();
             // 
-            // button1
+            // LogInBtn2
             // 
-            button1.Location = new Point(582, 277);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 2;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            LogInBtn2.BackColor = System.Drawing.Color.Black;
+            LogInBtn2.Font = new Font("Comic Sans MS", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LogInBtn2.ForeColor = System.Drawing.Color.MediumPurple;
+            LogInBtn2.Location = new Point(788, 302);
+            LogInBtn2.Name = "LogInBtn2";
+            LogInBtn2.Size = new Size(145, 71);
+            LogInBtn2.TabIndex = 2;
+            LogInBtn2.Text = "Log In";
+            LogInBtn2.UseVisualStyleBackColor = false;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(134, 174);
+            textBox1.BackColor = System.Drawing.Color.Black;
+            textBox1.ForeColor = System.Drawing.Color.White;
+            textBox1.Location = new Point(195, 149);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(184, 23);
             textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(162, 248);
+            textBox2.BackColor = System.Drawing.Color.Black;
+            textBox2.ForeColor = System.Drawing.Color.White;
+            textBox2.Location = new Point(195, 209);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
+            textBox2.Size = new Size(184, 23);
             textBox2.TabIndex = 4;
+            // 
+            // UsernameLbl
+            // 
+            UsernameLbl.AutoSize = true;
+            UsernameLbl.BackColor = System.Drawing.Color.Transparent;
+            UsernameLbl.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            UsernameLbl.ForeColor = System.Drawing.Color.Yellow;
+            UsernameLbl.Location = new Point(51, 141);
+            UsernameLbl.Name = "UsernameLbl";
+            UsernameLbl.Size = new Size(129, 30);
+            UsernameLbl.TabIndex = 5;
+            UsernameLbl.Text = "Username: ";
+            UsernameLbl.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // PasswordLbl
+            // 
+            PasswordLbl.BackColor = System.Drawing.Color.Transparent;
+            PasswordLbl.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            PasswordLbl.ForeColor = System.Drawing.Color.Lime;
+            PasswordLbl.Location = new Point(51, 201);
+            PasswordLbl.Name = "PasswordLbl";
+            PasswordLbl.Size = new Size(129, 44);
+            PasswordLbl.TabIndex = 6;
+            PasswordLbl.Text = "Password: ";
+            PasswordLbl.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // LoginBtn1
+            // 
+            LoginBtn1.BackColor = System.Drawing.Color.Black;
+            LoginBtn1.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LoginBtn1.ForeColor = System.Drawing.Color.BlueViolet;
+            LoginBtn1.Location = new Point(234, 302);
+            LoginBtn1.Name = "LoginBtn1";
+            LoginBtn1.Size = new Size(145, 71);
+            LoginBtn1.TabIndex = 7;
+            LoginBtn1.Text = "Log In";
+            LoginBtn1.UseVisualStyleBackColor = false;
+            // 
+            // OrLbl
+            // 
+            OrLbl.BackColor = System.Drawing.Color.Transparent;
+            OrLbl.Font = new Font("Comic Sans MS", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            OrLbl.ForeColor = System.Drawing.Color.Red;
+            OrLbl.Location = new Point(447, 181);
+            OrLbl.Name = "OrLbl";
+            OrLbl.Size = new Size(81, 42);
+            OrLbl.TabIndex = 8;
+            OrLbl.Text = "OR";
+            OrLbl.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // LoginLbl
+            // 
+            LoginLbl.AutoSize = true;
+            LoginLbl.BackColor = System.Drawing.Color.Transparent;
+            LoginLbl.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LoginLbl.ForeColor = System.Drawing.Color.Red;
+            LoginLbl.Location = new Point(81, 53);
+            LoginLbl.Name = "LoginLbl";
+            LoginLbl.Size = new Size(277, 30);
+            LoginLbl.TabIndex = 9;
+            LoginLbl.Text = "Already have an account?";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.BackColor = System.Drawing.Color.Transparent;
+            checkBox1.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox1.ForeColor = System.Drawing.Color.Cyan;
+            checkBox1.Location = new Point(51, 326);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(168, 27);
+            checkBox1.TabIndex = 10;
+            checkBox1.Text = "Keep me logged in";
+            checkBox1.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(93, 201);
+            label1.BackColor = System.Drawing.Color.Transparent;
+            label1.Font = new Font("Comic Sans MS", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = System.Drawing.Color.FromArgb(255, 128, 0);
+            label1.Location = new Point(123, 92);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 5;
-            label1.Text = "label1";
+            label1.Size = new Size(176, 35);
+            label1.TabIndex = 11;
+            label1.Text = "Log In Here: ";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(101, 286);
+            label2.BackColor = System.Drawing.Color.Transparent;
+            label2.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = System.Drawing.Color.FromArgb(255, 128, 0);
+            label2.Location = new Point(612, 53);
             label2.Name = "label2";
-            label2.Size = new Size(38, 15);
-            label2.TabIndex = 6;
-            label2.Text = "label2";
-            // 
-            // button2
-            // 
-            button2.Location = new Point(406, 326);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 7;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            label2.Size = new Size(254, 30);
+            label2.TabIndex = 12;
+            label2.Text = "Don't have an account?";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(139, 69);
+            label3.BackColor = System.Drawing.Color.Transparent;
+            label3.Font = new Font("Comic Sans MS", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = System.Drawing.Color.Yellow;
+            label3.Location = new Point(641, 92);
             label3.Name = "label3";
-            label3.Size = new Size(38, 15);
-            label3.TabIndex = 8;
-            label3.Text = "label3";
+            label3.Size = new Size(180, 35);
+            label3.TabIndex = 13;
+            label3.Text = "Register Now:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(159, 106);
+            label4.BackColor = System.Drawing.Color.Transparent;
+            label4.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = System.Drawing.Color.Lime;
+            label4.Location = new Point(528, 143);
             label4.Name = "label4";
-            label4.Size = new Size(38, 15);
-            label4.TabIndex = 9;
-            label4.Text = "label4";
+            label4.Size = new Size(215, 30);
+            label4.TabIndex = 14;
+            label4.Text = "Desired Username: ";
+            label4.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = System.Drawing.Color.Black;
+            textBox3.ForeColor = System.Drawing.Color.Red;
+            textBox3.Location = new Point(749, 151);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(184, 23);
+            textBox3.TabIndex = 15;
+            // 
+            // label5
+            // 
+            label5.BackColor = System.Drawing.Color.Transparent;
+            label5.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = System.Drawing.Color.Cyan;
+            label5.Location = new Point(569, 188);
+            label5.Name = "label5";
+            label5.Size = new Size(129, 44);
+            label5.TabIndex = 16;
+            label5.Text = "Password: ";
+            label5.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = System.Drawing.Color.Black;
+            textBox4.ForeColor = System.Drawing.Color.FromArgb(0, 0, 0, 7);
+            textBox4.Location = new Point(749, 196);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(184, 23);
+            textBox4.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = System.Drawing.Color.Black;
+            textBox5.ForeColor = System.Drawing.Color.White;
+            textBox5.Location = new Point(749, 252);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(184, 23);
+            textBox5.TabIndex = 18;
+            // 
+            // label6
+            // 
+            label6.BackColor = System.Drawing.Color.Transparent;
+            label6.Font = new Font("Comic Sans MS", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = System.Drawing.Color.FromArgb(192, 192, 255);
+            label6.Location = new Point(528, 246);
+            label6.Name = "label6";
+            label6.Size = new Size(194, 29);
+            label6.TabIndex = 19;
+            label6.Text = "Confirm Password: ";
+            label6.TextAlign = ContentAlignment.TopCenter;
             // 
             // LoginRegisterForm
             // 
@@ -113,15 +259,25 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1038, 450);
+            Controls.Add(label6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
             Controls.Add(label4);
             Controls.Add(label3);
-            Controls.Add(button2);
             Controls.Add(label2);
             Controls.Add(label1);
+            Controls.Add(checkBox1);
+            Controls.Add(LoginLbl);
+            Controls.Add(OrLbl);
+            Controls.Add(LoginBtn1);
+            Controls.Add(PasswordLbl);
+            Controls.Add(UsernameLbl);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Controls.Add(button1);
+            Controls.Add(LogInBtn2);
             Name = "LoginRegisterForm";
             Text = "Login/Register";
             ResumeLayout(false);
@@ -130,13 +286,23 @@
 
         #endregion
 
-        private Button button1;
+        private Button LogInBtn2;
         private TextBox textBox1;
         private TextBox textBox2;
+        private Label UsernameLbl;
+        private Label PasswordLbl;
+        private Button LoginBtn1;
+        private Label OrLbl;
+        private Label LoginLbl;
+        private CheckBox checkBox1;
         private Label label1;
         private Label label2;
-        private Button button2;
         private Label label3;
         private Label label4;
+        private TextBox textBox3;
+        private Label label5;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label6;
     }
 }
